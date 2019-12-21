124fd31fb8fd5353d5a090054ecae57cd4de4157
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2019-12-21 18:53:10 -0500

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+function analytics() {
+  // Detect Fonts Available
+  // Screen Width & Height Information
+  // Browser Build Number
+  // Browser Version
+  // Codec Detection
+  // WebGL
+}
+
+analytics();
